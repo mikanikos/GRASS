c81804eb5bb3c04965b6ce94ac480692beecaf82
Andrea Piccione <andrea.piccione@epfl.ch>
2019-03-15 00:10:27 +0100

    added interactive shell and partial support for some simple commands

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,5 @@
 #include <grass.h>
+#include <sys/wait.h>
 
 /*
  * Send a file to the server as its own thread
@@ -6,7 +7,8 @@
  * fp: file descriptor of file to send
  * d_port: destination port
  */
-void send_file(int fp, int d_port) {
+void send_file(int fp, int d_port)
+{
     // TODO
 }
 
@@ -17,10 +19,10 @@ void send_file(int fp, int d_port) {
  * d_port: destination port
  * size: the size (in bytes) of the file to recv
  */
-void recv_file(int fp, int d_port, int size) {
+void recv_file(int fp, int d_port, int size)
+{
     // TODO
 }
-
 
 /*
  * search all files in the current directory
@@ -28,47 +30,68 @@ void recv_file(int fp, int d_port, int size) {
  *
  * pattern: an extended regular expressions.
  */
-void search(char *pattern) {
+void search(char *pattern)
+{
     // TODO
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // TODO:
     // Make a short REPL to send commands to the server
     // Make sure to also handle the special cases of a get and put command
 
-    int sock; 
+    int sock;
 
     if (argc != 3)
         exit(0);
 
-    // CREATION 
-    sock = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sock < 0) {
+    // CREATION
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
         perror("creation failed");
-        exit(0); 
+        exit(0);
     }
-    
-    struct sockaddr_in s_addr;  
+
+    struct sockaddr_in s_addr;
     s_addr.sin_family = AF_INET;
     s_addr.sin_addr.s_addr = inet_addr(argv[1]);
     s_addr.sin_port = htons(atoi(argv[2]));
-  
-    if (inet_pton(AF_INET, argv[1], &s_addr.sin_addr) < 0)  
-    { 
-        perror("invalid address"); 
+
+    if (inet_pton(AF_INET, argv[1], &s_addr.sin_addr) < 0)
+    {
+        perror("invalid address");
         exit(0);
     }
 
-    // CONNECTION 
-    if (connect(sock, (struct sockaddr*)&s_addr, sizeof(s_addr)) < 0)  {
-        perror("connect failed");   
+    // CONNECTION
+    if (connect(sock, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0)
+    {
+        perror("connect failed");
         exit(0);
     }
 
-    printf("CONNECTED\n");
-    
-    while(true);
-  
-    close(sock); 
+    char buff[1024]; 
+    int n; 
+    while (true) { 
+        bzero(buff, sizeof(buff)); 
+        printf("grass> ");
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        
+        write(sock, buff, sizeof(buff)); 
+        
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Logged out\n"); 
+            break; 
+        } 
+        
+        bzero(buff, sizeof(buff)); 
+        read(sock, buff, sizeof(buff)); 
+        printf("%s", buff); 
+    } 
+
+    close(sock);
 }
--- src/server.cpp
+++ src/server.cpp
@@ -38,7 +38,36 @@ void recv_file(int fp, int sock, int size)
 // Server side REPL given a socket file descriptor
 void *connection_handler(void *sockfd)
 {
-    printf("Handling connection\n");
+    int sock = *(int *)sockfd;
+    FILE *fp;
+    char buff[1024], result[1024] = "";
+
+    while (true) {
+        bzero(result, 1024);
+        bzero(buff, 1024);
+
+        read(sock, buff, sizeof(buff));
+
+        if (strncmp("exit", buff, 4) == 0)
+        {
+            break;
+        }
+
+        fp = popen(buff, "r");
+        if (fp == NULL)
+        {
+            perror("command failed");
+            exit(0);
+        }
+
+        while (fgets(buff, sizeof(buff), fp) != NULL)
+        {
+            strcat(result, buff);
+        }
+        pclose(fp);
+
+        write(sock, result, sizeof(result));
+    }
 }
 
 /*
@@ -63,35 +92,46 @@ void parse_grass()
 
     char buffer[256];
 
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
         if (buffer[0] == '#' || buffer[0] == '\n')
             continue;
-        else {
+        else
+        {
             s = strtok(buffer, " ");
-            if (s != NULL) {
-                if (strcmp(s, "base") == 0) {
+            if (s != NULL)
+            {
+                if (strcmp(s, "base") == 0)
+                {
                     t = strtok(NULL, "\n");
-                    if (t != NULL) {;
+                    if (t != NULL)
+                    {
+                        ;
                         strcpy(curr_dir, t);
-                        printf("base: %s\n", curr_dir);
+                        //printf("base: %s\n", curr_dir);
                     }
                 }
-                else if (strcmp(s, "port") == 0) {
+                else if (strcmp(s, "port") == 0)
+                {
                     t = strtok(NULL, "\n");
-                    if (t != NULL) {
+                    if (t != NULL)
+                    {
                         strcpy(port, t);
-                        printf("port: %s\n", port);
+                        //printf("port: %s\n", port);
                     }
                 }
-                else if (strcmp(s, "user") == 0) {
+                else if (strcmp(s, "user") == 0)
+                {
                     char name[50], pass[50];
                     t = strtok(NULL, " ");
-                    if (t != NULL) {
+                    if (t != NULL)
+                    {
                         strncpy(name, t, 50);
                         t = strtok(NULL, "\n");
-                        if (t != NULL) {
+                        if (t != NULL)
+                        {
                             strncpy(pass, t, 50);
-                            printf("User: %s %s\n", name, pass);
+                            //printf("User: %s %s\n", name, pass);
                         }
                     }
                 }
@@ -105,33 +145,35 @@ void parse_grass()
 int main()
 {
     // Parse the grass.conf file
-    printf("Parsing grass.conf file...\n");
     parse_grass();
-    
+
     // Listen to the port and handle each connection
 
     // CREATION
     int sock;
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         perror("creation failed");
         exit(0);
     }
 
     struct sockaddr_in s_addr;
     s_addr.sin_family = AF_INET;
-    s_addr.sin_addr.s_addr =  inet_addr("127.0.0.1"); // htonl(INADDR_ANY);
+    s_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // htonl(INADDR_ANY);
     s_addr.sin_port = htons(atoi(port));
 
     // BIND
-    if ((bind(sock, (struct sockaddr *)&s_addr, sizeof(s_addr))) < 0) {
+    if ((bind(sock, (struct sockaddr *)&s_addr, sizeof(s_addr))) < 0)
+    {
         perror("bind failed");
         exit(0);
     }
 
     // START LISTENING
-    if ((listen(sock, 5)) < 0) {
+    if ((listen(sock, 5)) < 0)
+    {
         perror("listen failed");
         exit(0);
     }
@@ -145,8 +187,9 @@ int main()
         int sock_new;
 
         sock_new = accept(sock, (struct sockaddr *)&c_addr, (socklen_t *)&c_addr_len);
-        if (sock_new < 0) {
-            perror("accept failed");   
+        if (sock_new < 0)
+        {
+            perror("accept failed");
             exit(0);
         }
 
@@ -154,6 +197,6 @@ int main()
 
         close(sock_new);
     }
-    
+
     close(sock);
 }
